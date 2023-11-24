532d9354ff4110bb09f2c4d2428e1f58fbdcb566
osy <osy9757@gmail.com>
2023-11-25 08:33:52 +0900

    11월 4주차 토요일: 오수영 - [231125] 인기있는 아이스크림, 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기, 동물의 아이디와 이름

---
+++ syoh/sql/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -0,0 +1,5 @@
+select car_type, count(*) as cars
+from car_rental_company_car
+where options like '%통풍시트%' or options like '%열선시트%' or options like '%가죽시트%'
+group by car_type
+order by car_type asc;
