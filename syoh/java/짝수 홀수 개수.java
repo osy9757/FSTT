fb60c22f8b263384b0d2bd452c51a3a871864bee
osy9757 <osy9757@gmail.com>
2023-11-27 08:55:57 +0900

    11월 5주차 월요일 : 오수영 - [231127] 짝수 홀수 개수, 문자열을 정수로 변환하기, 배열 원소의 길이

---
+++ syoh/java/짝수 홀수 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0,0};
+        for (int num : num_list) {
+            if (num%2 == 0)
+                answer[0] += 1;
+            else
+                answer[1] += 1;
+        }
+        return answer;
+    }
+}
