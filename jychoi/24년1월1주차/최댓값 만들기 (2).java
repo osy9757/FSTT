22589be7225c96ba252129adc2b482577832d51c
jeong-yooon <chjy1819@gmail.com>
2024-01-07 23:43:16 +0900

    1월 1주차 일요일: 최정윤 - [240107] 최댓값 만들기 (2), 가장 큰 수 찾기

---
+++ jychoi/24년1월1주차/최댓값 만들기 (2).java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+
+        int num1 = numbers[0] * numbers[1];
+        int num2 = numbers[numbers.length-1] * numbers[numbers.length-2];
+
+        if(num1 < num2) {
+            return num2;
+        } else {
+            return num1;
+        }
+    }
+}
